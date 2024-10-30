2c8192a64db0135545fa07d49ac015facb508a21
Milan <milansonyofficial@gmail.com>
2024-10-30 17:20:48 +0530

    style: basic UI created

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -12,13 +12,22 @@ function ToDoApp() {
                                 <h1 className='text-center mb-5 head-text'>Todo List</h1>
                                 <h6 className='mb-3 day'>Wednesday</h6>
                             </div>
-                            <div className="input-group">
+                            <div className="input-group mb-3">
                                 <input type="text" class="form-control todo-input" placeholder="📝 Add your task" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                                    <button className="btn add-btn" type="button" id="button-addon2">ADD</button>
+                                <button className="btn add-btn" type="button" id="button-addon2">ADD</button>
                             </div>
+
+                            {/* list item */}
+                            <div class="input-group mb-3">
+                                <div class="input-group-text">
+                                    <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
+                                </div>
+                                <input type="text" className="form-control list-item" aria-label="Text input with checkbox" value={"hello"} readOnly/>
+                            </div>
+
                         </div>
+
                     </div>
-                    {/* list item */}
 
                 </div>
             </div>
