b7204bb45eaee7260b33d6f7bb2f35c54f73ca09
Milan <milansonyofficial@gmail.com>
2024-10-31 09:32:43 +0530

    style: basic UI created

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -9,12 +9,12 @@ function ToDoApp() {
                     <div className='row d-flex justify-content-center'>
                         <div className='col-lg-6 mt-5'>
                             <div>
-                                <h1 className='text-center mb-5 head-text'>Todo List</h1>
+                                <h1 className='text-center mb-5 head-text'><i class="bi bi-check2-circle"></i>&nbsp;Todo List</h1>
                                 <h6 className='mb-3 day'>Wednesday</h6>
                             </div>
                             <div className="input-group mb-3">
                                 <input type="text" className="form-control todo-input" placeholder="📝 Add your task" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                                <button className="btn add-btn" type="button" id="button-addon2">ADD</button>
+                                <button className="btn add-btn" type="button" id="button-addon2"><i class="bi bi-plus-circle-fill"></i>&nbsp;ADD</button>
                             </div>
 
                             {/* list item */}
